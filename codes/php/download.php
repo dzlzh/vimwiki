41ef98e20769d923d41d402088ea2e5884dabfe4
dzlzh <dzlzh@null.net>
2019-03-12 15:54:51 +0800

    :memo: Codes - PHP - download

--- codes/php/download.php
+++ codes/php/download.php
@@ -4,6 +4,6 @@ function download($data)
     header('Content-type: application/octet-stream');
     header('Accept-Ranges: bytes');
     header('Accept-Length:' . strlen($data));
-    header('Content-Disposition: attachment; filename=test');
+    header('Content-Disposition: attachment; filename="fileName";filename*=utf-8\'\'fileName');
     echo $data;
 }
